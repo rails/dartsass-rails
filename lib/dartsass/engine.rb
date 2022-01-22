140eb49158784d85fa5abffb6b76f224de1ad6d4
David Heinemeier Hansson <david@hey.com>
2022-01-22 13:57:13 +0100

    Style and spelling

--- lib/dartsass/engine.rb
+++ lib/dartsass/engine.rb
@@ -3,6 +3,6 @@ require "rails"
 module Dartsass
   class Engine < ::Rails::Engine
     config.dartsass = ActiveSupport::OrderedOptions.new
-    config.dartsass.stylesheets = { 'application.scss' => 'appliction.css' }
+    config.dartsass.stylesheets = { "application.scss" => "application.css" }
   end
 end
